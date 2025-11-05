e41f26c36e6bb168a9b21c622003f2720f1b5dda
KerbyLimitandy <kerby5677@gmail.com>
2025-11-05 15:28:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.lab_week_09
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LAB_WEEK_09Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Home()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Home(items: List<String>,) {
+    LazyColumn {
+        item {
+            Column (
+                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(id = R.string.enter_item))
+                TextField (
+                    value = "",
+                    keyboardOptions = KeyboardOptions()
+                )
+            }
+        }
+    }
+}
+
+//For displaying the UI without running the program
+@Preview(showBackground = true)
+@Composable
+fun PreviewHome() {
+    Home(ListOf("Tanu", "Tina", "Tono"))
+}

3453c3604ae3c0fc55054344264afe147bdcf5dd
KerbyLimitandy <kerby5677@gmail.com>
2025-11-05 15:52:26 +0700

    Commit No. 1 – Building Jetpack Compose UI

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -8,16 +8,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +35,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Home()
+                    val list = listOf("Tanu", "Tina", "Tono")
+                    Home(list)
                 }
             }
         }
@@ -48,8 +54,22 @@ fun Home(items: List<String>,) {
                 Text(text = stringResource(id = R.string.enter_item))
                 TextField (
                     value = "",
-                    keyboardOptions = KeyboardOptions()
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    onValueChange = {}
                 )
+
+                Button(onClick = { }) {
+                    Text(text = stringResource(id = R.string.button_click))
+                }
+            }
+        }
+
+        items(items) { item ->
+            Column(
+                modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = item)
             }
         }
     }
@@ -59,5 +79,5 @@ fun Home(items: List<String>,) {
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome() {
-    Home(ListOf("Tanu", "Tina", "Tono"))
+    Home(listOf("Tanu", "Tina", "Tono"))
 }
